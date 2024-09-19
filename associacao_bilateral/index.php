85d3b7e34884116f0273a35264581efc24e93a6c
GEOVANA CRISTINA VALENTIM BUENO <0200832311021>
2024-09-19 12:09:00 -0300

    Mostrando os fornecedores do produto - relação bilateral

--- associacao_bilateral/index.php
+++ associacao_bilateral/index.php
@@ -44,4 +44,32 @@
         $cont++;
     }
 
+    $fornecedor1 = new Fornecedor (
+        "Faber Castell", "Faber Castell", "22.222.222/0001-22", "(11) 997952290"
+    );
+    $fornecedor2 = new Fornecedor (
+        "Pilot", "Pilot", "33.333.333/0001-33", "(19) 997948990"
+    );
+
+    $produto4 = new Produto (
+        'Borracha', 'Borracha macia que não borra', 3.10, 2300, array($fornecedor, $fornecedor2, $fornecedor1)
+    );
+
+    echo "<h3>Produto</h3>";
+    echo "Nome: {$produto4 -> getNome()} <br>"; 
+    echo "Descrição: {$produto4 -> getDescricao()} <br>"; 
+    echo "Preço R$: " . number_format($produto4 -> getPreco(),2,",",".") .  "<br>"; 
+    echo "Estoque: {$produto4 -> getEstoque()} <br>";
+
+    echo "<h3>Fornecedores do produto</h3>";
+
+    foreach($produto4 -> getFornecedor() as $obj){
+        echo "<br>Razão Social: {$obj -> getRazaoSocial()} <br>";
+        echo "Nome Fantasia: {$obj -> getNomeFantasia()} <br>";
+        echo "CNPJ: {$obj -> getCnpj()} <br>";
+        echo "Telefone: {$obj -> getTelefone()} <br>";
+    
+        echo "__________________________________________________________<br>";
+    }
+
 ?>
